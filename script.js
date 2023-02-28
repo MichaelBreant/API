eb92df8d7bc7ead199017871f4517c59e852bb06
MichaelBreant <breant121@gmail.com>
2023-02-28 11:29:34 +0100

    API user

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ fetch('https://jsonplaceholder.typicode.com/users')
 })
 .then(users =>{
     users .forEach(user => {
-        const li =document.createElement("li")
+        const li =document.createElement("li");
+        const li1 =document.createElement("li");
+        const li2 =document.createElement("li");
         li.textContent=user.name;
         document.getElementById("list").appendChild(li);
+        li1.textContent=user.email;
+        document.getElementById("list").appendChild(li1);
+        li2.textContent=user.address.zipcode;
+        document.getElementById("list").appendChild(li2);
+        li2.style.marginBottom = "10px";
     });
 }).catch("ça marche pas");
 
