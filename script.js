c740a0b76f19e96d05afb34f0d8b49718cef3840
MichaelBreant <breant121@gmail.com>
2023-02-28 11:16:12 +0100

    API user

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
     users .forEach(user => {
         const li =document.createElement("li")
         li.textContent=user.name;
-        li.textContent=user.email;
-        li.textContent=user.zipcode;
         document.getElementById("list").appendChild(li);
     });
 }).catch("ça marche pas");
